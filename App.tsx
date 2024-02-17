d331d88fdc6507a58bb6510fdcb92ca90db4476b
Salah Darwish <100836109+Salah-Darwish@users.noreply.github.com>
2024-02-17 23:30:28 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Slider from './src/component/Slider';
+
+const App = () => {
+  return (
+    <View>
+      <Slider />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
